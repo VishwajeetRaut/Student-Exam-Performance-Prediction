d0c9f13350ed71cdd3521ce43bf3d8df4de4a67c
Vishwajeet_Raut <vishwajeetraut54@gmail.com>
2024-08-13 01:53:02 +0530

    updated code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+setup(
+    name='Student Performance Prediction',
+    version='0.0.1',
+    author='Vishwajeet',
+    author_email='vishwajeetraut54@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
